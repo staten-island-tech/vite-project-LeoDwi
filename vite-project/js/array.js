dee0ea1f3bfee2829b5018db391ac491324463c5
LeoDwi <leod13579@gmail.com>
2022-12-09 09:57:20 -0500

    Update array.js

--- vite-project/js/array.js
+++ vite-project/js/array.js
@@ -1,26 +1,48 @@
 export const menu = [
   {
-    itemName: "Burger",
-    inStock: "False",
-    price: "$6.99",
+    itemName: "Steamed Pork Dumplings",
+    inStock: "True",
+    price: "$3.99",
     vegetarian: "False",
-    wagyu: "False",
-    image: "",
+    image:
+      "https://www.thespruceeats.com/thmb/Rlz7p_8os-SmkAfOABoImBtfSTY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/panfried-pork-potstickers-694859-step-16-aad5e5bb9df5427185ab35d1ff6327c8.jpg",
+  },
+  {
+    itemName: "Pan Fried Dumplings",
+    inStock: "True",
+    price: "$3.99",
+    vegetarian: "False",
+    image:
+      "https://twoplaidaprons.com/wp-content/uploads/2020/05/Chinese-pork-dumplings-picking-up-a-dumpling-with-chopsticks.jpg",
+  },
+  {
+    itemName: "Soupy buns",
+    inStock: "True",
+    price: "$8.99",
+    vegetarian: "False",
+    image:
+      "https://www.chinasichuanfood.com/wp-content/uploads/2014/09/soup-dumpling-7.jpg",
+  },
+  {
+    itemName: "Pan Fried Buns",
+    inStock: "True",
+    price: "$3.99",
+    vegetarian: "False",
+    image:
+      "https://data.thefeedfeed.com/static/2020/02/19/15821517645e4db8544ab58.jpg",
   },
   {
     itemName: "Dominic",
     inStock: "True",
     price: "$1.99",
     vegetarian: "False",
-    wagyu: "True",
     image: "",
   },
   {
-    itemName: "Filet Mignon",
+    itemName: "",
     inStock: "True",
-    price: "$200",
+    price: "",
     vegetarian: "False",
-    wagyu: "True",
     image: "",
   },
   {
@@ -56,7 +78,6 @@ export const menu = [
     inStock: "",
     price: "$",
     vegetarian: "",
-    wagyu: "",
     image: "",
   },
 ];
